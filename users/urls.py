df78f99fa0926f201df1c447d05959bfb111a247
Pvl <ivanov.pavel.1307@gmail.com>
2023-10-23 01:18:03 +0200

    setup of users

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import UserListAPIView, UserRetrieveAPIView, UserCreateAPIView, UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path('', UserListAPIView.as_view(), name='user_list'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    path('create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
 ]
