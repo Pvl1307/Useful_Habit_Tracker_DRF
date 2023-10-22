9dff65c0879eb7f486e3db11f0633f0f20a3b702
Pvl <ivanov.pavel.1307@gmail.com>
2023-10-22 17:40:13 +0200

    urls for habits

--- habit/urls.py
+++ habit/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
+
 from habit.apps import HabitConfig
+from habit.views import HabitListAPIView, HabitRetrieveAPIView, HabitCreateAPIView, HabitUpdateAPIView, \
+    HabitDestroyAPIView
 
 app_name = HabitConfig.name
 
 urlpatterns = [
-
+    path('habit/', HabitListAPIView.as_view(), name='habit_list'),
+    path('habit/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit_detail'),
+    path('habit/create/', HabitCreateAPIView.as_view(), name='habit_create'),
+    path('habit/update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit_update'),
+    path('habit/delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='habit_delete'),
 ]
